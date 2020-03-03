cb1a9f7d0778ab598ce9fdc04af9e099e5233c38
c376W <mailfzc123@gmail.com>
2020-03-03 12:31:31 -0800

    Creating main section and border values.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'border-radius-previewer';
+  topLeft="";
+  topRight="";
+  bottomLeft="";
+  bottomRight="";
+  
+  
+  
+  changeTopLeft(){
+    let mainSec=<HTMLElement>document.querySelector(".main");
+    mainSec.style.borderTopLeftRadius=this.topLeft+"px";
+  }
+  changeTopRight(){
+    let mainSec=<HTMLElement>document.querySelector(".main");
+    mainSec.style.borderTopRightRadius=this.topRight+"px";
+  }
+  changeBottomLeft(){
+    let mainSec=<HTMLElement>document.querySelector(".main");
+    mainSec.style.borderBottomLeftRadius=this.bottomLeft+"px";
+  }
+  changeBottomRight(){
+    let mainSec=<HTMLElement>document.querySelector(".main");
+    mainSec.style.borderBottomRightRadius=this.bottomRight+"px";
+  }
 }
