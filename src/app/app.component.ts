dd0176fd74fc543b872c26b5903ac216ece52682
c376W <mailfzc123@gmail.com>
2020-03-04 16:17:54 -0800

    Adding copy to clipboard action.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   topRight="";
   bottomLeft="";
   bottomRight="";
+
   
   
   
@@ -28,5 +29,22 @@ export class AppComponent {
   changeBottomRight(){
     let mainSec=<HTMLElement>document.querySelector(".main");
     mainSec.style.borderBottomRightRadius=this.bottomRight+"px";
+  }
+  copyMessage(){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = "border-top-left-radius:"+this.topLeft+"px;"+"\n"
+    +"border-top-right-radius:"+this.topRight+"px;"+"\n"
+    +"border-bottom-left-radius:"+this.bottomLeft+"px;"+"\n"
+    +"border-bottom-right-radius:"+this.bottomRight+"px;";
+    
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 }
